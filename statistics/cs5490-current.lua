67b30a5a2b0acfea74d3657b7772901e30b1396c
Shawn Rutledge <s@ecloud.org>
2019-10-20 20:00:56 +0200

    use https with a user and password for influxdb logging

--- statistics/cs5490-current.lua
+++ statistics/cs5490-current.lua
@@ -1,12 +1,18 @@
 #!/usr/bin/lua
 local socket = require "socket"
-local http = require("socket.http")
+local https = require "ssl.https"
 local string = require "string"
 local port_name = "/dev/ttyS1"
 local out = io.stdout
 chip = require "cs5490"
 
-local serverUrl = "http://<ip-addr>:<port>/write?db=homeauto&rp=energy_rp"
+local influxServerAddr = ""
+local influxServerPort = 8086
+local influxUser = ""
+local influxPass = ""
+
+local serverUrl = string.format("https://%s:%d/write?db=homeauto&rp=energy_rp&u=%s&p=%s",
+		influxServerAddr, influxServerPort, influxUser, influxPass)
 
 local err = chip.open(port_name)
 if err ~= "" then
@@ -22,5 +28,5 @@ socket.sleep(0.9)
 local timestampNS = socket.gettime()*1000000000
 local current = currentScale * chip.readRegisterFixed0dot24(16, 6)
 local query = string.format("energy,location=earll2314,device=poolpump current=%f", current)
-local body, headers, code = http.request(serverUrl, query)
+local body, code, headers, status = https.request(serverUrl, query)
 out:write(string.format('PUTVAL "pool/exec-pool/current-pump" interval=30 N:%f\n', current))
